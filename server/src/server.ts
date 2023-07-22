4736bbb8dc7c2b34e21ec5b825df3f6968ff1358
coded1guy <grggmmnl@gmail.com>
2023-07-22 20:00:36 +0100

    update: added createMember and loginMember paths

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 
 import router from './router'
 import { checkAuthorization } from './utils/auth'
+import { createMember, logMemberIn } from './handlers/member'
 
 const app = express()
 
@@ -15,7 +16,10 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.json({ message: "welcome to my server" })
 })
-
+// member auth paths
+app.post('/createMember', createMember);
+app.post('/loginMember', logMemberIn);
+// auth-required paths
 app.use('/api', checkAuthorization, router)
 
 export default app
