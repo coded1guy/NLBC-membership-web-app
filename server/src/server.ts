fcca323c9e715e929ffc0ed6abdcea3ae43c2402
coded1guy <grggmmnl@gmail.com>
2023-07-27 14:24:05 +0100

    updated admin authentication route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { body } from "express-validator";
 
-import router from './router'
-import { checkAuthorization } from './utils/auth'
-import { createAdminInput, logAdminInInput, resolveValidation } from './utils/inputValidation'
-import { createMember, logMemberIn } from './handlers/member'
-import { createAdmin, logAdminIn } from './handlers/admin'
+import router from './router';
+import { checkAuthorization } from './utils/auth';
+import { createAdminInputValidation, logAdminInInputValidation } from './utils/validations/admin';
+import { resolveValidation } from './utils/validations/resolveValidation';
+import { createMember, logMemberIn } from './handlers/member';
+import { createAdmin, logAdminIn } from './handlers/admin';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: "welcome to my server" })
-})
+    res.json({ message: "welcome to my server" });
+});
 // member auth paths
 app.post('/createMember', createMember);
 app.post('/loginMember', logMemberIn);
 
 // admin auth paths
-app.post('/createAdmin', createAdmin)
-app.post('/logAdminIn', logAdminInInput, resolveValidation, logAdminIn)
+app.post('/createAdmin', createAdminInputValidation, resolveValidation, createAdmin);
+app.post('/logAdminIn', logAdminInInputValidation, resolveValidation, logAdminIn);
 
 // auth-required paths
-app.use('/api', checkAuthorization, router)
+app.use('/api', checkAuthorization, router);
 
-export default app
+export default app;
