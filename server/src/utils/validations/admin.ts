5b02fd8b81cf775890df2edd8347a36c9c2d1d69
coded1guy <grggmmnl@gmail.com>
2023-07-27 14:21:01 +0100

    fix: fixed the validation error for the admin login route changed the format of the validation checks to allow either username or email to be null, but not at the same time

--- server/src/utils/validations/admin.ts
+++ server/src/utils/validations/admin.ts
@@ -3,19 +3,45 @@ import { body } from "express-validator";
 // ALL ADMIN ROUTE INPUT VALIDATION
 // createAdmin route
 export const createAdminInputValidation = [
-    body('firstName', 'First name is required!').notEmpty().isString().escape().withMessage('First name must be a string!'),
-    body('lastName', 'Last name is required!').notEmpty().isString().escape().withMessage('Last name must be a string!'),
-    body('username', 'Username is required!').notEmpty().escape().withMessage('Username must be a string!'),
-    body('email', 'Email is required!').notEmpty().isEmail().escape().withMessage('Not a valid email address!'),
-    body('password', 'Password is required!').notEmpty().isStrongPassword().escape().withMessage('Password is not strong enough!'),
-    body('status', 'Status is required!').isIn([ 'active', 'inactive', 'deactivated' ]).escape().withMessage('Status must be a string and have a value of \'active\', \'inactive\' or \'deactivated\'!')
+    body('firstName', 'First name is required!').notEmpty().isString().escape()
+    .withMessage('First name must be a string!'),
+    body('lastName', 'Last name is required!').notEmpty().isString().escape()
+    .withMessage('Last name must be a string!'),
+    body('username', 'Username is required!').notEmpty().escape()
+    .withMessage('Username must be a string!'),
+    body('email', 'Email is required!').notEmpty().isEmail().escape()
+    .withMessage('Not a valid email address!'),
+    body('password', 'Password is required!').notEmpty().isStrongPassword().escape()
+    .withMessage('Password is not strong enough!'),
+    body('status', 'Status is required!').isIn([ 'active', 'inactive', 'deactivated' ]).escape()
+    .withMessage(
+        `Status must be a string and have a value of \'active\', \'inactive\' or \'deactivated\'!`
+    )
 ];
 // LogAdminIn route
 export const logAdminInInputValidation = [
-    body('username', 'Username is required!').notEmpty().escape().withMessage('Username must be a string!'),
-    body('email', 'Email is required!').notEmpty().isEmail().escape().withMessage('Not a valid email address!'),
-    body('password', 'Password is required!').notEmpty().isStrongPassword().escape().withMessage('Password is not strong enough!'),
-    body('status', 'Status is required!').isIn([ 'active', 'inactive', 'deactivated' ]).escape().withMessage('Status must be a string and have a value of \'active\', \'inactive\' or \'deactivated\'!')
+    body('username', 'Must be a string!').isString().optional({nullable: true, checkFalsy: true})
+    .custom((value, { req })=> {
+        let status:boolean;
+        if(!(req.body.email || value)) {
+            status = false;
+        } else {
+            status = true;
+        }
+        return status;
+    }).escape().withMessage('Username or Email must be present!'),
+    body('email', 'Must be an Email!').optional({nullable: true, checkFalsy: true}).isString()
+    .isEmail().custom((value, { req })=> {
+        let status:boolean;
+        if(!(req.body.username || value)) {
+            status = false;
+        } else {
+            status = true;
+        }
+        return status;
+    }).escape().withMessage('Email or Username must be present!'),
+    body('password', 'Password is required!').notEmpty().isString().escape()
+    .withMessage('Password must be a string!'),
 ];
 // update admin route
 // export const updateAdminInput = () => {
@@ -23,7 +49,9 @@ export const logAdminInInputValidation = [
 //     const lastName = body('lastName').isString().optional({nullable: true}).escape();
 //     const userName = body('username').isString().optional({nullable: true}).escape();
 //     const email = body('email').isEmail().optional({nullable: true}).escape();
-//     const password = body('password').isString().isStrongPassword().optional({nullable: true}).escape();
-//     const status = body('status').isIn([ 'active', 'inactive', 'deactivated' ]).optional({nullable: true}).escape();
+//     const password = body('password').isString().isStrongPassword().optional({nullable: true})
+//     .escape();
+//     const status = body('status').isIn([ 'active', 'inactive', 'deactivated' ])
+//     .optional({nullable: true}).escape();
 //     return [ firstName, lastName, userName, email, password, status ];
 // }
