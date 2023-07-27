b00a9254640a5070ca97e27bcc6328d2053a2ff8
coded1guy <grggmmnl@gmail.com>
2023-07-27 14:22:28 +0100

    updated the error message for the input validation resolver

--- server/src/utils/validations/resolveValidation.ts
+++ server/src/utils/validations/resolveValidation.ts
@@ -3,7 +3,7 @@ import { validationResult } from "express-validator";
 export const resolveValidation = (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        res.status(400).json({ message: "Bad Inputs.", errors: errors.array() })
+        res.status(400).json({ message: "Invalid Inputs.", errors: errors.array() })
     } else {
         next()
     }
