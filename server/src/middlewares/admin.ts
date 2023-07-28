f0b96179cebe1aa152c06925b7637d0f620a4c8e
coded1guy <grggmmnl@gmail.com>
2023-07-28 16:00:19 +0100

    feat: made the super user permission and restricted some routes to the permission

---
+++ server/src/middlewares/admin.ts
@@ -0,0 +1,25 @@
+import prisma from "../db"
+// must be admin protector
+export const mustBeMaster = async(req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const admin = await prisma.admin.findUnique({
+            where: {
+                id: req.user.id
+            }
+        })
+
+        if(admin.role === "super") {
+            next();
+        } else {
+            res.status(403).json({ message: "You are not allowed to access this resource." });
+            return;
+        }
+    } catch (e) {
+        res.status(404);
+        res.json(
+            { message: "An error occurred while validating permission to access this resource" }
+        );
+    }
+}
