01cc1c18c5629c1ccc714621736dfb3a2154ec7e
coded1guy <grggmmnl@gmail.com>
2023-08-03 17:19:36 +0100

    add: added a new check to the only-admin-accessible route

--- server/src/middlewares/admin.ts
+++ server/src/middlewares/admin.ts
@@ -1,23 +1,63 @@
 import prisma from "../db"
-// must be admin protector
-export const mustBeMaster = async(req, res, next) => {
+import { defineError } from "../utils/defineError";
+
+const errorScope:string = "admin", errorType:string = "access";
+const error404msg:string = "An error occurred while validating permission to access this resource.";
+
+// checks if logged in user is an admin
+export const isAdmin = async(req, res, next) => {
+    // error message handler
+    const sendError = (error:object | null, message:string, status:number) => {
+        const newError = defineError(errorScope, errorType, error, message);
+        console.error(newError);
+        res.status(status).json({ message, error: newError });
+    }
+    // check if the user id matches that of an admin
     try {
         const admin = await prisma.admin.findUnique({
             where: {
                 id: req.user.id
             }
         })
-
-        if(admin.role === "super") {
+        if(admin === null) {
+            sendError(null, "Only admins can access this resource.", 403);
+            return;
+        }
+        if(admin.status === "active") {
             next();
         } else {
-            res.status(403).json({ message: "You are not allowed to access this resource." });
+            sendError(null, "Only active admins can access this resource.", 403);
             return;
         }
     } catch (e) {
-        res.status(404);
-        res.json(
-            { message: "An error occurred while validating permission to access this resource" }
-        );
+        sendError(e, error404msg, 404);
+        return;
+    }
+}
+
+// checks if logged in user is a super admin
+export const isSuperAdmin = async(req, res, next) => {
+    // error message handler
+    const sendError = (error:object | null, message:string, status:number) => {
+        const newError = defineError(errorScope, errorType, error, message);
+        console.error(newError);
+        res.status(status).json({ message, error: newError });
+    }
+    // checks if the admin role is super
+    try {
+        const admin = await prisma.admin.findUnique({
+            where: {
+                id: req.user.id
+            }
+        })
+        if(admin.role === "super") {
+            next();
+        } else {
+            sendError(null, "Admin is not qualifies to access this resource.", 403);
+            return;
+        }
+    } catch (e) {
+        sendError(e, error404msg, 404);
+        return;
     }
 }
