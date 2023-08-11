9e416ce4df845ef42462c8fc719b36b34d333737
coded1guy <grggmmnl@gmail.com>
2023-08-11 14:35:38 +0100

    feat: added pagination to the data of the getAllAdmin function

--- server/src/handlers/admin.ts
+++ server/src/handlers/admin.ts
@@ -210,17 +210,39 @@ export const getAnAdmin = async(req, res, next) => {
 // Handler for the super admin to get all admins
 export const getAllAdmin = async(req, res, next) => {
     // declaring and initializing variables
-    let admin;
+    let admin, adminCount:number, lastId:string | null = null;
+    let { limit, cursorId } = req.query;
+    limit = +limit;
     // simple function for error handling while querying the db
     const sendError = function (type, error) {
         error = defineError(scope, type, error);
         next(error);
     }
+    // defining the database query options
+    let otherOptions:object = {};
+    if(cursorId) {
+        otherOptions = {
+            skip: 1,
+            cursor: { id: cursorId },
+            orderBy: { id: 'asc' },
+        };
+    } else {
+        otherOptions = {
+            orderBy: { id: 'asc' },
+        }
+    }
+    console.log(otherOptions);
     // querying the database
     try {
-        admin = await prisma.admin.findMany();
+        [admin, adminCount] = await prisma.$transaction([
+            prisma.admin.findMany({
+                take: limit | 10,
+                ...otherOptions
+            }),
+            prisma.admin.count(),
+        ])
 
-        if(admin === null) {
+        if(admin === null || admin.length < 1) {
             sendError("noList", null);
             return;
         }
@@ -228,10 +250,18 @@ export const getAllAdmin = async(req, res, next) => {
         sendError(defineCatchType(e, "get"), e);
         return;
     }
+    // get the last id of the set of admin returned to get the cursor for next page
+    if(admin.length === limit && admin.length < adminCount) {
+        lastId = admin[admin.length - 1].id;
+    }
     // success output
     res.status(200).json({
         message: "Admin list was gotten successfully.",
-        data: admin
+        result: {
+            total: adminCount,
+            lastId,
+            data: admin
+        }
     });
     return;
 }
