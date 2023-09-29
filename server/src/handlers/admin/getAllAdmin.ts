59ad8f1350a6e58739f25aecbb323b0115ba2ef5
coded1guy <grggmmnl@gmail.com>
2023-09-29 16:44:27 +0100

    refactor: getAllAdmin endpoint optimized the pagination and ordering of the return data added rule to not return passwords as part of the data

--- server/src/handlers/admin/getAllAdmin.ts
+++ server/src/handlers/admin/getAllAdmin.ts
@@ -1,68 +1,63 @@
-import prisma from "../../db";
-import { defineError, defineCatchType } from "../../utils/defineError";
-import { adminScope } from ".";
+import prisma from '../../db';
+import { defineError, defineCatchType } from '../../utils/defineError';
+import { adminScope, adminReturnData } from '.';
 
-const getAllAdmin = async(req, res, next) => {
-    // declaring and initializing variables
-    let admin, adminCount:number, lastId:string | null = null;
-    let { limit, cursorId } = req.query;
-    limit = +limit;
+const getAllAdmin = async (req, res, next) => {
+  // declaring and initializing variables
+  let admin,
+    adminCount: number,
+    lastId: string | null = null;
+  let { limit, cursorId } = req.query;
+  limit = +limit;
 
-    // simple function for error handling while querying the db
-    const sendError = function (type, error) {
-        error = defineError(adminScope, type, error);
-        next(error);
+  // simple function for error handling while querying the db
+  const sendError = function (type, error) {
+    error = defineError(adminScope, type, error);
+    next(error);
+  };
+
+  // defining the database query options
+  let queryOptions: object = {
+    take: limit | 20,
+    skip: 1,
+    orderBy: { createdAt: 'desc' },
+    select: { ...adminReturnData },
+  };
+  if (cursorId) {
+    queryOptions['cursor'] = cursorId;
+  }
+
+  // querying the database
+  try {
+    [admin, adminCount] = await prisma.$transaction([
+      prisma.admin.findMany({ ...queryOptions }),
+      prisma.admin.count(),
+    ]);
+
+    if (admin === null || admin.length < 1) {
+      sendError('noList', null);
+      return;
     }
-
-    // defining the database query options
-    let otherOptions:object = {};
-    if(cursorId) {
-        otherOptions = {
-            skip: 1,
-            cursor: { id: cursorId },
-            orderBy: { id: 'asc' },
-        };
-    } else {
-        otherOptions = {
-            orderBy: { id: 'asc' },
-        }
-    }
-    console.log(otherOptions);
-
-    // querying the database
-    try {
-        [admin, adminCount] = await prisma.$transaction([
-            prisma.admin.findMany({
-                take: limit | 10,
-                ...otherOptions
-            }),
-            prisma.admin.count(),
-        ])
-
-        if(admin === null || admin.length < 1) {
-            sendError("noList", null);
-            return;
-        }
-    } catch(e) {
-        sendError(defineCatchType(e, "get"), e);
-        return;
-    }
-
-    // get the last id of the set of admin returned to get the cursor for next page
-    if(admin.length === limit && admin.length < adminCount) {
-        lastId = admin[admin.length - 1].id;
-    }
-
-    // success output
-    res.status(200).json({
-        message: "Admin list was gotten successfully.",
-        result: {
-            total: adminCount,
-            lastId,
-            data: admin
-        }
-    });
+  } catch (e) {
+    sendError(defineCatchType(e, 'get'), e);
     return;
-}
+  }
+
+  // get the last id of the set of admin returned to get the cursor for next page
+  if (admin.length === limit && admin.length < adminCount) {
+    lastId = admin[admin.length - 1].id;
+  }
+
+  // success output
+  res.status(200).json({
+    message: 'Admin list was gotten successfully.',
+    result: {
+      total: adminCount,
+      lastId,
+      data: admin,
+    },
+  });
+  return;
+};
 
 export default getAllAdmin;
--- server/src/handlers/admin/index.ts
+++ server/src/handlers/admin/index.ts
@@ -1,9 +1,23 @@
 // declared error scope
-export const adminScope:string = "admin";
+export const adminScope: string = 'admin';
 
-export {default as createAdmin} from "./createAdmin";
-export {default as logAdminIn} from "./logAdminIn";
-export {default as getAnAdmin} from "./getAnAdmin";
-export {default as getAllAdmin} from "./getAllAdmin";
-export {default as updateAdmin} from "./updateAdmin";
-export {default as deleteAnAdmin} from "./deleteAnAdmin";
+export const adminReturnData = {
+  id: true,
+  firstName: true,
+  lastName: true,
+  username: true,
+  email: true,
+  password: false,
+  role: true,
+  status: true,
+  lastLoggedIn: true,
+  createdAt: true,
+  updatedAt: true,
+};
+
+export { default as createAdmin } from './createAdmin';
+export { default as logAdminIn } from './logAdminIn';
+export { default as getAnAdmin } from './getAnAdmin';
+export { default as getAllAdmin } from './getAllAdmin';
+export { default as updateAdmin } from './updateAdmin';
+export { default as deleteAnAdmin } from './deleteAnAdmin';
